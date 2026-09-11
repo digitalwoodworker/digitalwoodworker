a43b7d8b6f51d7144dd3fb9ddf16111af91b782c
Nithin <digitalwoodworker@gmail.com>
2026-09-11 20:19:21 +0530

    code chnages and webp updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://digitalwoodworker.in',
   base: '/',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()]
   }
